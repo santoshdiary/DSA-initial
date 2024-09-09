beb04e159dff7fda96c230fe6daa606787fdae4b
santoshdiary <160557558+santoshdiary@users.noreply.github.com>
2024-09-09 22:02:47 +0530

    fifth  commit

--- Sorting/insertionSorting.java
+++ Sorting/insertionSorting.java
@@ -1,7 +1,7 @@
 package Sorting;
 
 import java.util.Arrays;
-
+// in this algorithm we swap the previous element to the next element if next element is lower than previous
 public class insertionSorting {
     public static void main(String[] args) {
         int[] arr = {5, 3, 4, 1, 2};
